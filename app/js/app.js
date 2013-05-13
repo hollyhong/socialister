503c5049ec4998c4dba5de20db5b02e3b944e9e4
Henri Gabriel <henri@workforpizza.com>
2013-05-13 13:27:22 -0700

    Fix routing.

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('socialister', ['firebase', 'socialister.filters', 'socialister.services', 'socialister.directives', 'socialister.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/somthing', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/something', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/items', {templateUrl: 'partials/items.html', controller: 'ItemCtrl'});
     $routeProvider.otherwise({redirectTo: '/items'});
   }]);
