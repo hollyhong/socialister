2b2b17e90bf5d03632bf16a5ff2782aa803e977b
Henri Gabriel <henri@workforpizza.com>
2013-05-16 15:33:10 -0700

    Add Filter.

--- app/js/app.js
+++ app/js/app.js
@@ -6,5 +6,6 @@ angular.module('socialister', ['firebase', 'socialister.filters', 'socialister.s
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/something', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/items', {templateUrl: 'partials/items.html', controller: 'ItemCtrl'});
+    $routeProvider.when('/filter', {templateUrl: 'partials/filter.html', controller: 'FilterCtrl'});
     $routeProvider.otherwise({redirectTo: '/items'});
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,21 +4,28 @@
 
 angular.module('socialister.controllers', []).
   controller('MyCtrl1', [function() {
-
+    return;
   }])
   .controller('ItemCtrl', ['$scope', 'angularFire',
-    function MyCtrl($scope, angularFire) {
+    function ItemCtrl($scope, angularFire) {
       var url = 'https://wfp.firebaseio.com/items';
-      var promise = angularFire(url, $scope, 'items', []);
+      var promiseItems = angularFire(url, $scope, 'items', []);
 
       $scope.addItem = function () {
         if (!$scope.newItem.length) {
           return;
         }
-        $scope.items.push({
+
+        var itemRef = $scope.items.push({
           text: $scope.newItem,
           tags: $scope.newTags
         });
+
+        $scope.newTags.forEach(function(tag) {
+          var tagRef  = new Firebase('https://wfp.firebaseio.com/tags');
+          tagRef.child(tag).child($scope.newItem).set(itemRef.toString());
+        });
+
         $scope.newItem = '';
         $scope.newTags = '';
       };
@@ -28,4 +35,35 @@ angular.module('socialister.controllers', []).
       };
 
     }
-  ]);
+  ])
+  .controller('FilterCtrl', ['$scope', 'angularFire',
+    function FilterCtrl($scope, angularFire) {
+      var url = 'https://wfp.firebaseio.com/items';
+      var promise = angularFire(url, $scope, 'items', []);
+      promise.then(function() {
+        var items = $scope.items,
+          tags = {},
+          i,
+          t,
+          tag,
+          item;
+        for (i = items.length - 1; i >= 0; i--) {
+          item = items[i].text;
+          for (t = items[i]['tags'].length - 1; t >= 0; t--) {
+            tag = items[i]['tags'][t];
+            console.log(item + ':' + tag);
+            if (tags.hasOwnProperty(tag)) {
+              tags[tag].push(item);
+            }
+            else {
+              tags[tag] = [item];
+              console.log(tags);
+            }
+          }
+        }
+        $scope.items = items;
+        $scope.tags = tags;
+      });
+    }
+  ])
+;
