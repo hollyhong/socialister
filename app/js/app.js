aa7586eff93c5afe3fb7bf4cd6312c27bbd7440f
Holly Hong <holly.hong@gmail.com>
2013-06-01 23:40:28 -0700

    Added optional way to view lists

--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,11 @@
 // Declare app level module which depends on filters, and services
 angular.module('socialister', ['firebase', 'socialister.filters', 'socialister.services', 'socialister.directives', 'socialister.controllers']).
   config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/categories', {templateUrl: 'partials/categories.html', controller: 'MainCtrl'});
     $routeProvider.when('/items', {templateUrl: 'partials/items.html', controller: 'MainCtrl'});
     $routeProvider.when('/filter', {templateUrl: 'partials/filter.html', controller: 'MainCtrl'});
     $routeProvider.when('/feed', {templateUrl: 'partials/feed.html', controller: 'MainCtrl'});
+    $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: 'MainCtrl'});
     $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'MainCtrl'});
     $routeProvider.when('/send', {templateUrl: 'partials/send.html', controller: 'MainCtrl'});
     $routeProvider.when('/suggest', {templateUrl: 'partials/suggest.html', controller: 'MainCtrl'});
